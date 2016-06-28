f05e9051ae071028c3dae7715bcc826bff328e22
Michael Golfi <t-micgo@microsoft.com>
2016-06-28 14:23:11 -0700

    Added boilerplate for dialog, packages for Bot Builder

---
+++ Alfredo/Dialog/RestaurantDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Alfredo.Resource;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Alfredo
+{
+    [Serializable]
+    [LuisModel(Constants.AppId, Constants.SubscriptionId)]
+    public class RestaurantDialog : LuisDialog<int> // TODO Replace with actual object later
+    {
+        [LuisIntent("find_restaurant")]
+        public async Task FindRestaurant(IDialogContext context, LuisResult result)
+        {
+        }
+    }
+}
