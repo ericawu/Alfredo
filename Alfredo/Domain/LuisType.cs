265e8777e847596d4e7ed88ac80af39c82dd6cac
Erica Wu <erica15wu@gmail.com>
2016-06-29 11:15:02 -0700

    Added LuisType

---
+++ Alfredo/Domain/LuisType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alfredo.Domain
+{
+    public class LuisType
+    {
+        public const string Menu = "Menu";
+        public const string Cafe = "Cafe";
+
+    }
+}
